fe707030d0e1666e9472b517ac9418a72c7ebebf
Wesche D <28940651+SoftwareDeveloper007@users.noreply.github.com>
2018-03-03 08:08:49 -0800

    1st Commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from urllib import *
+from urllib.parse import urlparse, urljoin
+from urllib.request import urlopen
+import urllib.request
+import requests
+from io import BytesIO
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+from lxml import html
+from datetime import date, datetime, timedelta
+from dateutil.relativedelta import *
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
+
+def scraping(first_name="", last_name="", driver=None):
+
+    url = "http://uwf.edu/search/people/?q={}%20{}".format(first_name, last_name)
+    if driver is None:
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+        driver = webdriver.Chrome(chrome_options=chrome_options, executable_path='WebDriver/chromedriver.exe')
+        driver.maximize_window()
+
+    try:
+        driver.get(url)
+        row = driver.find_elements_by_css_selector("tr.person-result > td")
+        name = row[0]
+        position = row[1]
+        address = row[3]
+        phone_num = row[4]
+        mail = row[5]
+    except:
+        name = ""
+        position = ""
+        address = ""
+        phone_num = ""
+        mail = ""
+
+    print(name)
+    print(position)
+    print(address)
+    print(phone_num)
+    print(mail)
+
+    return [position, address, phone_num, mail]
+
+scraping(first_name="SUSAN", last_name="AMES")
